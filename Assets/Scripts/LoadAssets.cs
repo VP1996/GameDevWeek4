5968b9af768d175590ec5dfe0333e3763c6f7b4a
Vladimir <vposhvv@yandex.ru>
2020-08-19 16:33:53 +1000

    60 percent complete

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+
+    public GameObject redObj;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
