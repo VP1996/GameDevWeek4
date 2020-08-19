399a77ef9102b951a18378c6f070b92959111bd0
Vladimir <vposhvv@yandex.ru>
2020-08-19 17:44:34 +1000

    save2

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    private int i;
+
+    public GameObject sph;
+
+    public Renderer rend;
+
+
+
+    void Start()
+    {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(sph.name + ++i);
+    }
+}
