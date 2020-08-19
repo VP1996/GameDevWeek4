9e3a6a74823ede685de974560a1203d64aa4100e
Vladimir <vposhvv@yandex.ru>
2020-08-19 17:21:40 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
 
+  
+
     public GameObject redObj;
 
+    [SerializeField]
+    private GameObject blueObj;
     
     void Start()
     {
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
         
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
-    
-    void Update()
-    {
-        
-    }
+   
+
 }
